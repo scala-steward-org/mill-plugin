307926bcacc4f9a4297b2bbdb20a12fefb6bfea8
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2025-10-05 09:42:03 +0200

    Add getRootModule scaladoc

--- plugin/src-mill1/mill/scalasteward/internal/internal.scala
+++ plugin/src-mill1/mill/scalasteward/internal/internal.scala
@@ -1,5 +1,10 @@
 package mill.scalasteward.internal
 
 import mill.api.Evaluator
+import mill.api.internal.RootModule0
 
-def getRootModule(ev: Evaluator) = ev.rootModule
+/**
+ * Expose private[mill] def rootModule: RootModule0
+ * in mill.api.Evaluator
+ */
+def getRootModule(ev: Evaluator): RootModule0 = ev.rootModule
