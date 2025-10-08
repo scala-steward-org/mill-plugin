6ff1f793fdc563529a429c2d443c0631d985cc94
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2025-10-08 09:34:09 +0000

    Update Scaladoc
    Co-authored-by: Tobias Roeser <le.petit.fou@web.de>

--- plugin/src-mill1/mill/scalasteward/internal/internal.scala
+++ plugin/src-mill1/mill/scalasteward/internal/internal.scala
@@ -6,5 +6,6 @@ import mill.api.internal.RootModule0
 /**
  * Expose private[mill] def rootModule: RootModule0
  * in mill.api.Evaluator
+ * see: https://github.com/com-lihaoyi/mill/issues/5948
  */
 def getRootModule(ev: Evaluator): RootModule0 = ev.rootModule
